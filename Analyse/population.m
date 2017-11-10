7eb692ae5e8d09720eacdc55ed4d12d5ba896f3a
kaushik-l <jklakshm@bcm.edu>
2017-11-10 11:18:29 -0600

    plot tuning to events and continuous

--- Analyse/population.m
+++ Analyse/population.m
@@ -9,6 +9,13 @@ classdef population < handle
     
     %%
     methods
+        %% class constructor
+        function this = population()
+            this.units = [];
+            this.singleunits = [];
+            this.multiunits = [];
+            this.lfps = [];
+        end
         %% function to add sessions
         function AnalysePopulation(this,units,unittype,behaviours,prs)
             this.(unittype).stats = AnalysePopulation(units,behaviours.trials,behaviours.stats,prs);
